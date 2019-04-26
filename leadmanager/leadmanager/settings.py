5d327337bbb4cc22d583588d7c778ec5cde942c0
Hassani <hassanisnr@gmail.com>
2019-04-26 08:32:01 -0400

    Changing django database config

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -65,16 +65,8 @@ WSGI_APPLICATION = 'leadmanager.wsgi.application'
 
 # Database
 
-DATABASES = {
-    'default': {
-        'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', cast=int)
-    }
-}
+DATABASES['default'] = os.environ.get('DATABASE_URL')
+
 
 # Password validation
 
